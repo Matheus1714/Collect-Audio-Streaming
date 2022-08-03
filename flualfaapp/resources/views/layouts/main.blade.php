f57eb53d338c49edc1f123966cb68a707da49773
matheus1714 <matheussilvamartins1714@gmail.com>
2022-08-03 13:21:30 -0300

    init file base design

---
+++ flualfaapp/resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="/img/favicon.png">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>

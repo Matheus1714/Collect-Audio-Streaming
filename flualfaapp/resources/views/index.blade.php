8fbfd5c6a78685326f6ba5ebcd26e881d291a243
matheus1714 <matheussilvamartins1714@gmail.com>
2022-08-04 01:14:12 -0300

    remove js and add only tags

--- flualfaapp/resources/views/index.blade.php
+++ flualfaapp/resources/views/index.blade.php
@@ -4,59 +4,31 @@
 
 @section('content')
 
-<audio id="player" controls></audio>
+<div class="content">
+  <div class="card">
+    <div class="title content">
+      <p>Collect Audio</p>
+    </div>
+    <div class="player content">
+      <audio id="player" controls></audio>
+    </div>
+    <div class="controls">
+      <div class="button-start content">
+        <button id="start">Start Record</button>
+      </div>
+      <div class="button-stop content">
+        <button id="stop" disabled>Stop Record</button>
+      </div>
+      <div class="link content">
+        <a id="download">Download</a>
+      </div>
+      <div class="send content">
+        <button id="send">Send To Server</button>
+      </div>
+    </div>
+  </div>
+</div>
 
-<a id="download">Download</a>
-<button id="start">Start</button>
-<button id="stop">Stop</button>
-
-<script>
-
-  const player = document.getElementById('player');
-  const downloadLink = document.getElementById('download');
-  const stopButton = document.getElementById('stop');
-  const startBUtton = document.getElementById('start')
-
-  handleSucess = (stream) => {
-    const options = {mimeType: 'audio/webm'};
-    const mediaRecorder = new MediaRecorder(stream, options);
-
-    startBUtton.addEventListener('click', _ => {
-      console.log('start record')
-      mediaRecorder.start();
-    });
-
-    const audioChunks = [];
-
-    mediaRecorder.addEventListener("dataavailable", event => {
-      audioChunks.pop();
-      audioChunks.push(event.data);
-    });
-
-    mediaRecorder.addEventListener("stop", () => {
-      console.log('create audio Blocb object')
-      const audioBlob = new Blob(audioChunks);
-      const audioUrl = URL.createObjectURL(audioBlob);
-
-      const audio = new Audio(audioUrl);
-      player.src = audioUrl
-    });
-
-    mediaRecorder.addEventListener('stop', () => {
-      downloadLink.href = URL.createObjectURL(new Blob(audioChunks));
-      downloadLink.download = 'acetest.wav';
-    })
-
-    stopButton.addEventListener('click', _ => {
-        console.log('stop record')
-        mediaRecorder.stop();
-    });
-  }
-
-  navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(handleSucess);
-
-</script>
-
+<script src="js/Audio.js"></script>
     
 @endsection
